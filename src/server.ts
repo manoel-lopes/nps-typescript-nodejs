49dd969a9643da9e8ffb6bb9c4f6d88a2726d834
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-04-08 22:16:21 -0300

    starting the project configuration

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import  express from 'express'
+// const routes = require('./routes')
+
+// require('./db')
+
+const app = express()
+const port = 3333
+
+// app.use(cors());
+app.use(express.json())
+// app.use(routes)
+
+app.listen(
+    port,
+    () => console.log(`The server is running on http://localhost:${port}`))
