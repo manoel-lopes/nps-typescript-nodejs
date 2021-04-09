8cd3ba24b579473969b9a9653d45ea066fe9212e
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-04-09 00:14:06 -0300

    adding validations to create a user

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -9,30 +9,30 @@ class UserController {
 
     const userRepository = getRepository(User)
 
-    // if (!name || !email) {
-    //   return resp.status(400).json({ error: 'Blank field not allowed!' })
-    // }
+    if (!name || !email) {
+      return resp.status(400).json({ error: 'Blank field not allowed!' })
+    }
 
-    // const isValidEmail = email.includes('@') && email.includes('.')
+    const isValidEmail = email.includes('@') && email.includes('.')
 
-    // if (!isValidEmail) {
-    //   return resp.status(400).json({ error: 'Invalid email!' })
-    // }
+    if (!isValidEmail) {
+      return resp.status(400).json({ error: 'Invalid email!' })
+    }
 
     const user = userRepository.create({
       name, 
       email
     })
 
-    // const emailAlreadyRegistered = await userRepository.findOne({
-    //   where: {
-    //     email
-    //   }
-    // })
+    const emailAlreadyRegistered = await userRepository.findOne({
+      where: {
+        email
+      }
+    })
 
-    // if (emailAlreadyRegistered) {
-    //   return resp.status(400).json({ error: 'Email already in use!' })
-    // }
+    if (emailAlreadyRegistered) {
+      return resp.status(400).json({ error: 'Email already in use!' })
+    }
 
     await userRepository.save(user)
 
