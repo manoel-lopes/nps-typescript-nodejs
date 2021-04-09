95ecba13c796269a611b2a8dab16613d1e6d31e5
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-04-09 00:12:55 -0300

    creating the store method to create a User

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -1,10 +1,42 @@
 import { Request, Response } from 'express'
+import { getRepository } from 'typeorm'
+
+import { User } from '../models/User'
 
 class UserController {
-  async store(request: Request, response: Response) {
-    const body = request.body
-    console.log(body)
-    return response.send()
+  async store(req: Request, resp: Response) {
+    const { name = '', email = '' } = req.body
+
+    const userRepository = getRepository(User)
+
+    // if (!name || !email) {
+    //   return resp.status(400).json({ error: 'Blank field not allowed!' })
+    // }
+
+    // const isValidEmail = email.includes('@') && email.includes('.')
+
+    // if (!isValidEmail) {
+    //   return resp.status(400).json({ error: 'Invalid email!' })
+    // }
+
+    const user = userRepository.create({
+      name, 
+      email
+    })
+
+    // const emailAlreadyRegistered = await userRepository.findOne({
+    //   where: {
+    //     email
+    //   }
+    // })
+
+    // if (emailAlreadyRegistered) {
+    //   return resp.status(400).json({ error: 'Email already in use!' })
+    // }
+
+    await userRepository.save(user)
+
+    return resp.json(user)
   }
 }
 
