5b5bd8a5889bde994c58ab869890e2c6a8c0a0da
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-04-08 22:23:17 -0300

    creating routes and excluding unnecessary folder

---
+++ src/routes.ts
@@ -0,0 +1,4 @@
+import express from 'express'
+
+export const routes = express.Router()
+// creating routes and excluding unnecessary folder
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import  express from 'express'
+// import routes from './routes'
 // const routes = require('./routes')
 
 // require('./db')
