57b13ce98a7ee1d444ce756eb1bb436ca4bc7a0b
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-04-13 11:40:52 -0300

    refactoring

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,9 @@
 import 'reflect-metadata'
 import express from 'express'
-import { routes } from './routes'
 
+import { routes } from './routes'
 import createConnection from './db'
   
-require('dotenv').config({
-  path: process.env.NODE_ENV?.match('test') ? '.env.test' : '.env'
-})
-
 createConnection()
 const app = express()
 
