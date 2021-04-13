1213ab40abfb6437ff1b852176916ef1629db4c9
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-04-13 10:21:18 -0300

    refactoring test configurations

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,9 @@
-const test = !!process.env.NODE_ENV
-
 module.exports = {
-  type: test ? 'sqlite' : 'postgres',
+  type: 'postgres',
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: test ? './src/db/db.sqlite' : process.env.DB_NAME,
+  database: process.env.DB_NAME,
   entities: ['./src/app/models/**.ts'],
   migrations: ['./src/db/migrations/**.ts'],
   cli: {
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,18 @@
-import { Connection, createConnection } from 'typeorm'
+import { Connection, createConnection, getConnectionOptions } from 'typeorm'
 
 export default async(): Promise<Connection> => {
-  return createConnection()
+  const isNodeEnvSet = process.env.NODE_ENV
+  const defaultOptions = await getConnectionOptions()
+
+  const type = isNodeEnvSet ? 'sqlite' : defaultOptions.type
+  const database = isNodeEnvSet 
+    ? './src/__tests__/db_test.sqlite'
+    : defaultOptions.database
+
+  return createConnection(
+    Object.assign(defaultOptions, {
+      type,
+      database
+    })
+  )
 }
