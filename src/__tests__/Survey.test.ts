be45b619c9e48a378c3f6d7587ad2a6342a4a96e
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-04-13 11:17:23 -0300

    creating the survey method tests

---
+++ src/__tests__/Survey.test.ts
@@ -0,0 +1,36 @@
+import request from 'supertest'
+import { app } from '../app'
+
+import createConnection from '../db'
+
+describe('Surveys', () => {
+  beforeAll(async() => {
+    const conn = await createConnection()
+    await conn.runMigrations()
+  })
+
+  it('Should not be able to create a new survey without pass title', async() => {
+    const resp = await request(app).post('/surveys').send({
+      description: 'survey description'
+    })
+
+    expect(resp.status).toBe(400)
+  })
+
+  it('Should not be able to create a new survey without pass description', async() => {
+    const resp = await request(app).post('/surveys').send({
+      title: 'survey'
+    })
+
+    expect(resp.status).toBe(400)
+  })
+
+  it('Should be able to create a new survey', async() => {
+    const resp = await request(app).post('/surveys').send({
+      title: 'survey',
+      description: 'survey description'
+    })
+
+    expect(resp.status).toBe(201)
+  })
+})
