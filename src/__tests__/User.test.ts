5d75bd4ca8454ffe8c5256493b4128135ad1fd32
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-04-13 10:38:33 -0300

    finishing the User tests

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -9,6 +9,31 @@ describe('Users', () => {
     await conn.runMigrations()
   })
 
+  it('Should not be able to create a new user without pass name', async() => {
+    const resp = await request(app).post('/users').send({
+      email: 'user@email.com'
+    })
+
+    expect(resp.status).toBe(400)
+  })
+
+  it('Should not be able to create a new user without pass email', async() => {
+    const resp = await request(app).post('/users').send({
+      name: 'user'
+    })
+
+    expect(resp.status).toBe(400)
+  })
+
+  it('Should not be able to create a new user with a not valid email', async() => {
+    const resp = await request(app).post('/users').send({
+      name: 'user',
+      email: 'useremail'
+    })
+
+    expect(resp.status).toBe(400)
+  })
+
   it('Should be able to create a new user', async() => {
     const resp = await request(app).post('/users').send({
       name: 'user',
@@ -17,7 +42,7 @@ describe('Users', () => {
 
     expect(resp.status).toBe(201)
   })
-
+  
   it('Should not be able to create a new user with exists email', async() => {
     const resp = await request(app).post('/users').send({
       name: 'user',
