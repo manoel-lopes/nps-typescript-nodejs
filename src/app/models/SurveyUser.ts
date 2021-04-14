6d5c7919f2a0a78932126969f81e277679be90be
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-04-14 09:58:57 -0300

    creating survey user model and repository

---
+++ src/app/models/SurveyUser.ts
@@ -0,0 +1,30 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm'
+import { v4 as uuid } from 'uuid'
+
+@Entity('surveys')
+export class SurveyUser {
+  
+  @PrimaryColumn()
+  readonly id: string
+
+  @Column()
+  user_id: string
+  
+  @Column()
+  survey_id: string
+
+  @Column()
+  value: number
+  
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  update_at: Date
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid()
+    }
+  }
+}
