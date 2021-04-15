47dba50c13bd26cff294c34b62e1af0764a81e5d
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-04-15 00:50:13 -0300

    changing the tests database from sqlite to postgres

--- src/__tests__/integration/User.test.ts
+++ src/__tests__/integration/User.test.ts
@@ -1,4 +1,5 @@
 import request from 'supertest'
+import { getConnection } from 'typeorm'
 
 import { app } from '../../app'
 import createConnection from '../../db'
@@ -7,6 +8,12 @@ describe('Users', () => {
   beforeAll(async() => {
     const conn = await createConnection()
     await conn.runMigrations()
+  })
+
+  afterAll(async() => {
+    const conn = getConnection()
+    await conn.dropDatabase()
+    await conn.close()
   })
 
   it('Should not be able to create a new user without pass name', async() => {
